1700d2d2a30b1ce796d5191fca605700e5098a12
Himanshu Bhatt <bhatthimanshu8053@gmail.com>
2026-08-18 18:53:12 +0530

    Added Code 1

---
+++ CODE 1/code1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork failed\n");
+    }
+    else if (pid == 0)
+    {
+        printf("This is Child Process\n");
+        printf("Child PID = %d\n", getpid());
+    }
+    else
+    {
+        printf("This is Parent Process\n");
+        printf("Parent PID = %d\n", getpid());
+    }
+
+    return 0;
+}
